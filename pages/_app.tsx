a579ee5bb0d7eb71aae5fb2c67cd4852481860b0
XarTya <xartya@gmail.com>
2022-10-27 02:48:05 +0300

    header animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
 import type { AppProps } from 'next/app';
 import Layout from '@/components/Layout';
 import '@/styles/index.scss';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
+        <>
+            <Head>
+                <title>BIBLIOTHECA Café</title>
+            </Head>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </>
     );
 }
