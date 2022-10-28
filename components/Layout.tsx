bdecd0560a37885b9fbc46894238dc2b0f39e2e4
XarTya <xartya@gmail.com>
2022-10-28 14:10:32 +0300

    app preview animation update

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,13 +2,13 @@ import Logo from '@/components/UI/icons/Logo';
 
 export default function Layout({ children }) {
     return (
-        <>
+        <div id='wrapper'>
             <header>
                 <Logo />
             </header>
             <main>
                 {children}
             </main>
-        </>
+        </div>
     );
 }
